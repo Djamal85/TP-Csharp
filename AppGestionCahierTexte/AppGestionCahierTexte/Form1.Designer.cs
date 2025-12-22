239b64c523ec0958cda0a26aa6a113eec1f46168
Djamal85 <taoufikdjamal85@gmail.com>
2025-12-22 19:45:37 +0000

    Version1

---
+++ AppGestionCahierTexte/AppGestionCahierTexte/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace AppGestionCahierTexte
+{
+    partial class frmConnexion
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIdentifiant = new System.Windows.Forms.TextBox();
+            this.txtMotDePasse = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSeConnecter = new System.Windows.Forms.Button();
+            this.btnQuitter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Identifiant";
+            // 
+            // txtIdentifiant
+            // 
+            this.txtIdentifiant.Location = new System.Drawing.Point(49, 96);
+            this.txtIdentifiant.Name = "txtIdentifiant";
+            this.txtIdentifiant.Size = new System.Drawing.Size(517, 22);
+            this.txtIdentifiant.TabIndex = 1;
+            // 
+            // txtMotDePasse
+            // 
+            this.txtMotDePasse.Location = new System.Drawing.Point(50, 202);
+            this.txtMotDePasse.Name = "txtMotDePasse";
+            this.txtMotDePasse.Size = new System.Drawing.Size(517, 22);
+            this.txtMotDePasse.TabIndex = 2;
+            this.txtMotDePasse.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mot de passe";
+            // 
+            // btnSeConnecter
+            // 
+            this.btnSeConnecter.Location = new System.Drawing.Point(419, 288);
+            this.btnSeConnecter.Name = "btnSeConnecter";
+            this.btnSeConnecter.Size = new System.Drawing.Size(148, 38);
+            this.btnSeConnecter.TabIndex = 3;
+            this.btnSeConnecter.Text = "&Se Connecter";
+            this.btnSeConnecter.UseVisualStyleBackColor = true;
+            this.btnSeConnecter.Click += new System.EventHandler(this.btnSeConnecter_Click);
+            // 
+            // btnQuitter
+            // 
+            this.btnQuitter.Location = new System.Drawing.Point(50, 288);
+            this.btnQuitter.Name = "btnQuitter";
+            this.btnQuitter.Size = new System.Drawing.Size(105, 38);
+            this.btnQuitter.TabIndex = 4;
+            this.btnQuitter.Text = "&Quitter";
+            this.btnQuitter.UseVisualStyleBackColor = true;
+            this.btnQuitter.Click += new System.EventHandler(this.btnQuitter_Click);
+            // 
+            // frmConnexion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 495);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnQuitter);
+            this.Controls.Add(this.btnSeConnecter);
+            this.Controls.Add(this.txtMotDePasse);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtIdentifiant);
+            this.Controls.Add(this.label1);
+            this.Name = "frmConnexion";
+            this.Text = "Gestion Cahier de texte";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIdentifiant;
+        private System.Windows.Forms.TextBox txtMotDePasse;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSeConnecter;
+        private System.Windows.Forms.Button btnQuitter;
+    }
+}
+
